7dceda05515ff68911a02d3f230f847b9acdeca9
deadman96385 <seanhoyt963@gmail.com>
2015-01-04 15:15:53 -0500

    Liquify

--- liquid.mk
+++ liquid.mk
@@ -2,18 +2,18 @@
 TARGET_SCREEN_HEIGHT := 2048
 TARGET_SCREEN_WIDTH := 1536
 
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_tablet_wifionly.mk)
+# Inherit some common Liquid stuff.
+$(call inherit-product, vendor/liquid/config/common_tablet.mk)
 
 # Enhanced NFC
-$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
+$(call inherit-product, vendor/liquid/config/nfc_enhanced.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/htc/flounder/aosp_flounder.mk)
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := flounder
-PRODUCT_NAME := cm_flounder
+PRODUCT_NAME := liquid_flounder
 PRODUCT_BRAND := google
 PRODUCT_MODEL := Nexus 9
 PRODUCT_MANUFACTURER := htc
