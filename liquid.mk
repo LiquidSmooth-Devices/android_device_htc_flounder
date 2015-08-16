365fa4c131fc8173697011622d0d2d5b10658eeb
CPA-Poke <brock.shahan@gmail.com>
2015-08-16 17:46:38 -0400

    Liquify

--- liquid.mk
+++ liquid.mk
@@ -4,11 +4,11 @@
 TARGET_SCREEN_HEIGHT := 2048
 TARGET_SCREEN_WIDTH := 1536
 
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/slim/config/common_full_tablet_wifionly.mk)
+# Inherit some common Liquid stuff.
+$(call inherit-product, vendor/liquid/config/common_tablet_wifionly.mk)
 
 # Enhanced NFC
-$(call inherit-product, vendor/slim/config/nfc_enhanced.mk)
+$(call inherit-product, vendor/liquid/config/nfc_enhanced.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/htc/flounder/aosp_flounder.mk)
@@ -21,13 +21,13 @@ KERNEL_TOOLCHAIN_PREFIX := aarch64-linux-android-
 TARGET_KERNEL_SOURCE := kernel/htc/flounder
 TARGET_KERNEL_CONFIG := flounder_defconfig
 BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
-BOARD_KERNEL_CMDLINE := androidboot.selinux=enforcing
+BOARD_KERNEL_CMDLINE := androidboot.selinux=permissive
 
 # Extra Packages
 PRODUCT_PACKAGES += \
     com.android.nfc_extras
 
 ## Device identifier. This must come after all inclusions
-PRODUCT_NAME := slim_flounder
+PRODUCT_NAME := liquid_flounder
 PRODUCT_BRAND := google
 PRODUCT_MODEL := Nexus 9
